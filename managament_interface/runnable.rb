ef10fbb9aa831abcb6b305327b4d9d0aa6be5d15
Manuel Arno Korfmann <manu@korfmann.info>
2019-06-24 17:18:21 +0200

    2d app for bird's eye view

--- managament_interface/runnable.rb
+++ managament_interface/runnable.rb
@@ -1,13 +1,30 @@
 require 'ruby2d'
 
 # Set the window size
-set width: 300, height: 200
+set width: 400, height: 400
 
-# Create a new shape
-s = Square.new
+set title: "Fun first"
 
-# Give it some color
-s.color = 'red'
+Text.new('Managables', y: 0)
+    Text.new('  Games', y: 20)
+        Text.new('    Game Aided Manufacturing', y: 40)
+
+        
+    Text.new('  Services', y: 60)
+        Text.new('    Error Web App', y: 80)
+        Text.new('    Vision/OCR', y: 100)
+        Text.new('    Livestream Interactive', y: 120)
+
+
+on :mouse_up do |event|
+    # x and y coordinates of the mouse button event
+    case event.button
+    when :left
+        puts event.x
+        puts event.y
+    end
+end
+
 
 # Show the window
 show
