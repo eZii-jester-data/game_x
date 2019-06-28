3af23f2f1221aade3f2b20a35c21c6b2bfaf8567
Manuel Arno Korfmann <manu@korfmann.info>
2019-06-28 16:32:35 +0200

    ...

--- managables/services/vision-api/functions/root/handler.rb
+++ managables/services/vision-api/functions/root/handler.rb
@@ -1,5 +1,8 @@
 require 'tempfile'
 require 'json'
+require 'byebug'
+require "google/cloud/vision"
+require "base64"
 
 
 def handler event
@@ -7,30 +10,47 @@ def handler event
 
 	google_json_contents = JSON.parse(event.context)["google_json_contents"]
 
-	t.write(google_json_contents)
+
+	File.open(t.path, 'w') do |file|
+		file.write(google_json_contents)
+	end
 
 	ENV['GOOGLE_APPLICATION_CREDENTIALS'] = t.path
 
+	base_64_image = JSON.parse(event.body)["data"].gsub("data:image/jpeg;base64,", '')
+
+	image_path = base_64_image_to_local_tempfile_path(base_64_image)
+
+	result = ocr_document_image(image_path)
 
 	response = {
-		'message' => annotate_image
+		'message' => result
 	}
+
 	render json: response
 end
 
+def ocr_document_image(image_path)
+	image_annotator = Google::Cloud::Vision::ImageAnnotator.new
 
-require "google/cloud/vision"
+	response = image_annotator.document_text_detection image: image_path
 
+	text = ""
+	response.responses.each do |res|
+		res.text_annotations.each do |annotation|
+			text << annotation.description
+		end
+	end
 
-def annotate_image
-	image_annotator_client = Google::Cloud::Vision::ImageAnnotator.new
-	gcs_image_uri = "gs://gapic-toolkit/President_Barack_Obama.jpg"
-	source = { gcs_image_uri: gcs_image_uri }
-	image = { source: source }
-	type = :FACE_DETECTION
-	features_element = { type: type }
-	features = [features_element]
-	requests_element = { image: image, features: features }
-	requests = [requests_element]
-	response = image_annotator_client.batch_annotate_images(requests)
+	return text
+end
+
+def base_64_image_to_local_tempfile_path(base_64_image)
+	t = Tempfile.new(['image', '.jpg'])
+
+	base_64_decoded_image = Base64.decode64(base_64_image)
+
+	File.binwrite(t.path, base_64_decoded_image)
+
+	return t.path
 end
